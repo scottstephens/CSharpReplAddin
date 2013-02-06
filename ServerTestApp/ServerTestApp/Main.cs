34cfef8c79962049ea8af8c1fb5177026aff041d
Scott Stephens <stephens.js@gmail.com>
2013-02-05 22:38:56 -0600

    Improve simple test app a tiny bit

--- ServerTestApp/ServerTestApp/Main.cs
+++ ServerTestApp/ServerTestApp/Main.cs
@@ -10,6 +10,8 @@ namespace ServerTestApp
 			var proxy = new CSharpReplServerProxy(33333);
 			proxy.Start();
 			var output = proxy.evaluate("var x = 10;");
+			output = proxy.evaluate("for (int ii = 0; ii < 3; ii++) { Console.WriteLine(ii); }");
+
 //			var repl = new CSharpRepl();
 //			var output = repl.evaluate("10*10;");
 			Console.WriteLine(output.ResultType.ToString());
