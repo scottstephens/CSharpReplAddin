28a7e2204845bf05b971917dd9038cad31aec11e
Scott Stephens <stephens.js@gmail.com>
2013-05-14 17:52:56 -0500

    Create ReplServer versions for all architectures, clean up solution a bit

---
+++ CSharpReplServer32/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpReplServer64
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MonoDevelop.CSharpRepl.CSharpReplServer.Run(args);
+        }
+    }
+}
